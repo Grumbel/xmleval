b08f1b7e1bc77b58f7a0538ada8f56f45e8c0a5b
Ingo Ruhnke <grumbel@gmail.com>
2015-04-18 22:38:22 +0200

    Renamed *.?xx to *.?pp

--- src/xml_eval.cpp
+++ src/xml_eval.cpp
@@ -19,8 +19,7 @@
 
 #include <iostream>
 #include <ClanLib/core.h>
-#include "xml_eval.hxx"
-#include "string_converter.hxx"
+#include "xml_eval.hpp"
 
 namespace XMLEval {
 
@@ -31,7 +30,7 @@ int lookup(const std::string& name)
 {
   int value = 0;
   
-  if (from_string(name, value))
+  if (CL_String::from(name, value))
     {
       return value;
     }
