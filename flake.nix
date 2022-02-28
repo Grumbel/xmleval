f32e792f002ea90e208101e06779cd7e4f0882ee
Ingo Ruhnke <grumbel@gmail.com>
2022-02-28 18:23:48 +0100

    Add Nix flake build files

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "XML based toy programming language";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    clanlib.url = "gitlab:grumbel/clanlib-1.0";
+    clanlib.inputs.nixpkgs.follows = "nixpkgs";
+    clanlib.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, clanlib }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          xmleval = pkgs.stdenv.mkDerivation {
+            pname = "xmleval";
+            version = "0.2.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.pkgconfig
+            ];
+            buildInputs = [
+              clanlib.defaultPackage.${system}
+            ];
+           };
+        };
+        defaultPackage = packages.xmleval;
+      });
+}
